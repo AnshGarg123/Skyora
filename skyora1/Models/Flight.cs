6bb1f7aac49dc1a329f990854ad52fe928eec112
Ansh <Ansh.Garg@Evalueserve.com>
2026-04-30 10:15:03 +0530

    'skyora'

---
+++ skyora1/Models/Flight.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace skyora1.Models
+{
+    public class Flight
+    {
+        [Key]
+        public int FlightId { get; set; }
+        public string FlightNo { get; set; } = string.Empty;
+        public string Source { get; set; } = string.Empty;
+        public string Destination { get; set; } = string.Empty;
+        public DateTime DepartureTime { get; set; }
+        public DateTime ArrivalTime { get; set; }
+
+        public int TotalBusinessSeats { get; set; }
+        public int TotalEconomySeats { get; set; }
+        public int AvailableBusinessSeats { get; set; }
+        public int AvailableEconomySeats { get; set; }
+
+        public decimal BusinessPrice { get; set; }
+        public decimal EconomyPrice { get; set; }
+        public ICollection<Booking> Bookings { get; set; } = new List<Booking>();
+
+    }
+}
---
+++ skyora1/Models/Passenger.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace skyora1.Models
+{
+    public class Passenger
+    {
+
+        [Key]
+        public int PassengerId { get; set; }
+        [ForeignKey(nameof(PassengerId))]
+        public int BookingId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+        public string Gender { get; set; } = string.Empty;
+    }
+}
