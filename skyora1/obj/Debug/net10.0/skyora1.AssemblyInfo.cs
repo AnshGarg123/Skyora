0719f10ab451de01b700a8be13ed7039e8a51a23
Prasoon <Prasoon.Mishra@evalueserve.com>
2026-05-01 10:19:35 +0530

    added flight

--- skyora1/obj/Debug/net10.0/skyora1.AssemblyInfo.cs
+++ skyora1/obj/Debug/net10.0/skyora1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("skyora1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a92220330e6a4d4e2e3c14a98d184bd43b271be3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dda2eaff4193cbe47a32d8e2ac692e1912f6dd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("skyora1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("skyora1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
