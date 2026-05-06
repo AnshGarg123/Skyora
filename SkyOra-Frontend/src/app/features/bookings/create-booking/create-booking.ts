d89f622c15d700cf26247d313863bf9d9bbeb101
Prasoon <Prasoon.Mishra@evalueserve.com>
2026-05-06 12:26:12 +0530

    Added Angular frontend to project

---
+++ SkyOra-Frontend/src/app/features/bookings/create-booking/create-booking.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-booking',
+  imports: [],
+  templateUrl: './create-booking.html',
+  styleUrl: './create-booking.css',
+})
+export class CreateBooking {}
