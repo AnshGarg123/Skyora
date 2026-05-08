49b20c35ce52a4d2abe1323e826ccde3986a1336
prasooncodes <119727537+prasooncodes@users.noreply.github.com>
2026-05-08 10:46:07 +0530

    Revert "Prasoon component"

--- SkyOra-Frontend/src/app/features/bookings/create-booking/create-booking.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-create-booking',
-  imports: [],
-  templateUrl: './create-booking.html',
-  styleUrl: './create-booking.css',
-})
-export class CreateBooking {}
